cc555486a484a151be1300e14d3a7a62b395edcd
Tedi Setiandi <58543180+tedi25@users.noreply.github.com>
2025-12-12 20:49:11 +0700

    feat: authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { redirect } from "next/navigation"
 import postgres from "postgres"
 import { z } from "zod"
 import { DeleteInvoice } from "../ui/invoices/buttons"
+import { signIn } from "@/auth"
+import { AuthError } from "next-auth"
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" })
 const FormSchema = z.object({
@@ -28,6 +30,25 @@ export type State = {
     status?: string[]
   }
   message?: string | null
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials."
+        default:
+          return "Something went wrong."
+      }
+    }
+    throw error
+  }
 }
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
